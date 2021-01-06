890f3a139dcc66b0db13a0ea1605226ebd3d13a4
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-01-06 18:14:27 +0100

    1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        String myName = "Bartosz";
+        int myAge = 29;
+        char familyName = 'S';
+        System.out.println("Hello, world!");
+    }
+}
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        if (leapYear = 366){
+            System.out.println("This is leapYear");
+        }else if (leapYear > 366){
+            System.out.println("This is not leapYear");
+        }else{
+            System.out.println("This is not leapYear")
+        }
+    }
