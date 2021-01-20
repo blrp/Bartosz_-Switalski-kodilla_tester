df2ecc7e3030514efa697febc27b69781a2c5923
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-01-20 20:38:08 +0100

    user

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,23 +1,42 @@
 public class User {
-    public static void main(String[] args) {
-        String userName;
-        int userAge;
+    String UserName;
+    int UserAge;
+    public User (String Username, int UserAge){
+        this.UserName = Username;
+        this.UserAge = UserAge;
 
-        User anna = new User("Anna", 20);git
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "UserName='" + UserName + '\'' +
+                ", UserAge=" + UserAge +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        User anna = new User("Anna", 20);
         User betty = new User("Betty", 33);
         User carl = new User("Carl", 58);
         User david = new User("David", 13);
         User eva = new User("Eva", 18);
         User frankie = new User("Frankie", 45);
-
         User[] users = {anna, betty, carl, david, eva, frankie};
-        //System.out.println(frankie.getAge());
-    }public int averageAge (User[] users){
         int sum = 0;
-        for (int i = 0; i < users.length; i++) ;
-        int i = 0;
-        sum += users.length[i];
-        return (users.length == 0) ? 0 : (sum / users.length);
+        for (int i = 0; i < users.length; i++) {
+            sum = sum + users[i].UserAge;
+        }
+        double avg = sum / users.length;
+        for (int i = 0; i < users.length; i++) {
+          int age =  users[i].UserAge;
+          if ( age < avg ){
+              System.out.println(users[i]);
+          }
+        }
+
+
     }
 }
 
