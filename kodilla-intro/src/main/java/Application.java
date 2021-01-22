4038769649d57c1dee6a33992f728d08ac69ee77
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-01-22 17:47:37 +0100

    UserA

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -2,7 +2,7 @@ public class Application {
     public static void main(String[] args) {
         UserA userA = new UserA("Adam", 40.5, 178);
         System.out.println(UserA.name + " " + UserA.age + " " + UserA.height);
-        UserA.showInfo();
+        userA.showInfo();
     }
 
 }
