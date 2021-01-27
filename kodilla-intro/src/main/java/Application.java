ed99c623d49284ccfe8df5c7f8b35fe76358696f
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-01-27 18:32:35 +0100

    UserA

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,7 +1,9 @@
 public class Application {
+
+
     public static void main(String[] args) {
         UserA userA = new UserA("Adam", 40.5, 178);
-        System.out.println(UserA.name + " " + UserA.age + " " + UserA.height);
+        System.out.println("UserA");
         userA.showInfo();
     }
 
--- kodilla-intro/src/main/java/UserA.java
+++ kodilla-intro/src/main/java/UserA.java
@@ -1,18 +1,20 @@
 public class UserA {
 
-    static String name = "Adam";
-    static double age = 40.5;
-    static double height = 178;
+    String UserName;
 
-    public UserA(String name, double age, double height) {
-        this.name = name;
-        this.age = age;
-        UserA.height = height;
+     double UserAge;
+
+     double UserHeight;
+
+    public UserA(String UserName, double UserAge, double UserHeight) {
+        this.UserName = UserName;
+        this.UserAge = UserAge;
+        this.UserHeight = UserHeight;
     }
 
-    public static void showInfo() {
-        if (name != null) {
-            if (age > 30 && height > 160) {
+    public void showInfo() {
+        if (UserName != null) {
+            if (UserAge > 30 && UserHeight > 160) {
                 System.out.println("User is older than 30 and higher than 160cm");
             } else {
                 System.out.println("User is 30 (or younger) or 160cm (or shorter)");
