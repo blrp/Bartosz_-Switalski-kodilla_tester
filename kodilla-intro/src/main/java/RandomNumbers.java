4149dea32c6a387573908ba073e4000a29a0a432
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-01-27 21:06:49 +0100

    RandomNumbers

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -2,12 +2,37 @@ import java.util.Random;
 public  class RandomNumbers {
     int min = 30;
     int max = 0;
-    public void getMin() {
-        return;
+    public int getMin() {
+        return min;
 
     }
-    public void getMax() {
-        return;
+    public int getMax() {
+
+        return max;
     }
+    public void randomNumbers() {
+        Random random =new Random ();
+        int result = 0;
+        int randomNumber = random.nextInt(31);
+        int sum =0;
+
+        while (sum <= 5000) {
+            int temp =random.nextInt(31);
+            sum = sum + temp;
+            result++;
+            System.out.println(temp + "+" + "=" + sum );
+            if (temp < min) {
+                min = temp;
+                System.out.println("minimalna wylosowana to " + "" + min);
+
+            }
+            if (temp > max) {
+                max = temp;
+                System.out.println("maksymalna wylosowana to" + " "+ max);
+            }
+        }
+        System.out.println(result);
+    }
+
 
 }
