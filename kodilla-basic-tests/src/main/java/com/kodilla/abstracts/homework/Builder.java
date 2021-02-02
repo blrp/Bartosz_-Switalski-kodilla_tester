46169f469ba771892803bb9953613b0f16528b55
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-02-02 19:30:47 +0100

    person

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Builder.java
@@ -0,0 +1,9 @@
+package com.kodilla.abstracts.homework;
+
+public class Builder extends Job{
+
+    @Override
+    public void showResponsibilities() {
+        System.out.println("Builid");
+    }
+}
--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -2,25 +2,43 @@ package com.kodilla.abstracts.homework;
 
 public class Person extends Job{
 
+
+    private static Object TaxiDriver;
     private String firstName;
     private int age;
     private  Job job;
 
-    public Person(String jarek, int age, TaxiDriver taxiDriver) {
+    public Person(String jarek, int i, TaxiDriver taxiDriver) {
+
+
+    }
+
+    public Person() {
 
     }
 
 
     public void showPersonResponsibilities() {
-        System.out.println(firstName + "has to ");
-        job.showResponsibilities();
-        System.out.println("to earn for living");
+        System.out.println("Jestem TaxiDriver zawoze ludzi" + job);
+        System.out.println("Jestem Builder buduje domy" + job);
+
     }
 
     public static void main(String[] args) {
-        Person jarek = new Person("jarek", 50, new TaxiDriver());
+        Person jarek = new Person();
+        jarek.firstName = " Jarek";
+        jarek.age = 30;
+
+        Person marek = new Person();
+        marek.firstName = "Marek";
+        marek.age = 35;
+
+
+
+
 
         jarek.showPersonResponsibilities();
+        marek.showPersonResponsibilities();
 
 
 
