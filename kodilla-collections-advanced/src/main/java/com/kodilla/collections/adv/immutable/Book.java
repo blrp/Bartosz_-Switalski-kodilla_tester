624a0bde453b1fa52cda038fc0d550b5abc34e4b
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-02-08 21:11:17 +0100

    zad5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
@@ -0,0 +1,14 @@
+package com.kodilla.collections.adv.immutable;
+public final class Book {                       // [1]
+    protected final String author;
+    protected final String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public final String getAuthor() { return  author; }
+
+    public final String getTitle() { return title; }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/ImmutableTask.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class ImmutableTask {
+    public static void main(String[] args) {
+        Task task = new Task ("homework", 50 );
+
+    }
+}
