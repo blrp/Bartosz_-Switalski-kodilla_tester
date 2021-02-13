b2b6fe025675a551770784c881899a55ece15fd1
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-02-13 18:07:15 +0100

    zad6.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepository {
+    public static List<Task> getTasks(){
+        List<Task> taskList = new ArrayList<>();
+
+
+        taskList.add(new Task("Pay of the phone bill", LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 7)));
+        taskList.add(new Task("Buy new laptop", LocalDate.of(2020, 5, 15), LocalDate.of(2020, 5, 16)));
+        taskList.add(new Task("Send documents to costumer", LocalDate.of(2020, 5, 31), LocalDate.of(2020, 6, 20)));
+        return taskList;
+    }
+}
