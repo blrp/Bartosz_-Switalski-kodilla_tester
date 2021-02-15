4a8a988b4eef416142e4db081eecde05fbf8c8fc
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-02-15 18:40:38 +0100

    zad6.2

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Teacher.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Teacher.java
@@ -2,12 +2,22 @@ package com.kodilla.optional.homework;
 
 public class Teacher {
     String name;
+    Teacher teacher;
 
-    public Teacher(String name) {
+    public Teacher(String name, Teacher teacher) {
         this.name = name;
+        this.teacher = teacher;
+    }
+
+    public Teacher(Object witold) {
+
     }
 
     public String getName() {
         return name;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
     }
 }
