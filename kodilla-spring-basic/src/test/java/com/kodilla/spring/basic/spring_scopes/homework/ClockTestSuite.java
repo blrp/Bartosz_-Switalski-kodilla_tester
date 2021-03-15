79da114eb185c550ffb036f676b9ccaf896e7c1f
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-03-15 21:32:48 +0100

    zad9.5

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -0,0 +1,27 @@
+
+package com.kodilla.spring.basic.spring_scopes.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.time.LocalTime;
+
+@SpringBootTest
+class ClockTestSuite {
+
+
+
+    @Test
+    public void shouldCreateDifferentTimes() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_scopes");
+        Clock firstTimeBean = (Clock) context.getBean("clock", LocalTime.of(23,25,55));
+        Clock secondTimeBean = (Clock) context.getBean("clock", LocalTime.of(23,25,55));
+        Clock thirdTimeBean = (Clock) context.getBean("clock", LocalTime.of(11,14,17));
+        Assertions.assertNotEquals(firstTimeBean,secondTimeBean);
+        Assertions.assertNotEquals(firstTimeBean,thirdTimeBean);
+        Assertions.assertNotEquals(thirdTimeBean,secondTimeBean);
+        Assertions.assertEquals(firstTimeBean,firstTimeBean);
+    }
+}
