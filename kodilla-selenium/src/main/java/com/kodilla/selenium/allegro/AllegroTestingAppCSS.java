2fbad357e22299a62f9aae0d9a9a72314a276f25
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-04-12 19:29:33 +0200

    zad13.5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCSS.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCSS.java
@@ -8,30 +8,37 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class AllegroTestingAppCSS {
-    static WebDriver driver;
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.get("https://www.allegro.pl");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.cssSelector("img[alt='zamknij']"))).click();
 
-        Search("Elektronika", "mavic mini");
+        WebElement searchField = driver.findElement(By.cssSelector("#gh-cat-box>select"));
+        Select categorySelect = new Select(searchField);
+        categorySelect = new Select(searchField);
+        categorySelect.selectByValue("293");
 
-        driver.quit();
+
+        WebElement inputField = driver.findElement(By.cssSelector("#gh-ac-box2>input"));
+        inputField.sendKeys("Mavic mini");
+        inputField.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li>h2")));
+
+
+        List<WebElement> element = driver.findElements(By.cssSelector("div>ul[class*='srp-results srp-list clearfix']>li"));
+
+        System.out.println("----------------------------------------");
+        System.out.println("Ilość wyświetlonych produktów wynosi: " + element.size());
+        System.out.println("----------------------------------------");
+        System.out.println("Informacje o produkcie wyświetlonym jako pierwszy znajdują sie poniżej: " + "\n" + element.get(0).getText());
+        System.out.println("----------------------------------------");
+
     }
 
-    public static void Search(String categoryName, String searchString)
-    {
-        SelectCategory(categoryName);
-        WebElement searchBox = driver.findElement(By.cssSelector("input[name='string']"));
-        searchBox.sendKeys(searchString);
-        searchBox.submit();
-    }
-    public static void SelectCategory(String categoryName)
-    {
-        By categoryDropdownLocator = By.cssSelector("select[aria-label='Kategoria i opcje wyszukiwania']");
-        new Select(driver.findElement(categoryDropdownLocator)).selectByVisibleText(categoryName);
-    }
+
 }
