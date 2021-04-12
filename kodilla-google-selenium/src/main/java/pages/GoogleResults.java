74e98fa47dad773048a52610bb99991afc7c0f39
Bartosz Świtalski <bartosz.switalski@o2.pl>
2021-04-12 20:52:51 +0200

    zad13.6

---
+++ kodilla-google-selenium/src/main/java/pages/GoogleResults.java
@@ -0,0 +1,33 @@
+package pages;
+
+import java.util.List;
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleResults extends AbstractPage {
+
+    @FindBy(css = "div[class='g']")                  // [1]
+    private List<WebElement> results;                // [2]
+
+    public GoogleResults(WebDriver driver) {         // [3]
+        super(driver);                                // [4]
+        PageFactory.initElements(this.driver, this);  // [5]
+    }
+
+    public void iSeeResults() {
+        System.out.println("I see results");
+        System.out.println(results.size());
+    }
+    public WebElement oneRandomResult(){
+        Random random = new Random();
+        int randomPageId = random.nextInt(results.size());
+        WebElement webElement = results.get(randomPageId);
+        System.out.println(webElement.getTagName());
+        System.out.println(webElement);
+        return results.get(randomPageId);
+    }
+}
